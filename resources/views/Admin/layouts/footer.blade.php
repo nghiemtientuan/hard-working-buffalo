90b40911fa65b082e105f3a6c4dcc27e37fc61af
Nghiem Tien Tuan <nghiem.tien.tuan@sun-asterisk.com>
2020-04-04 18:12:13 +0700

    init component admin dashboard

---
+++ resources/views/Admin/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer text-muted">
+    &copy; . <a href="#"></a>
+</div>
---
+++ resources/views/Admin/layouts/scripts.blade.php
@@ -0,0 +1,9 @@
+<script src="{{ asset('bower_components/assets/Admin/js/plugins/visualization/c3/c3.min.js') }}"></script>
+<script src="{{ asset('bower_components/assets/Admin/js/core/libraries/jquery.min.js') }}"></script>
+<script src="{{ asset('bower_components/assets/Admin/js/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('bower_components/assets/Admin/js/treejs/jstree.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('bower_components/assets/Admin/js/toastr/toastr.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ asset(mix('js/admin.js')) }}"></script>
+
+@yield('script')
